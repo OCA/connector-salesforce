1d0bf30353c42efa12c43054bb15866d9d6984d6
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2019-11-08 09:52:27 +0100

    [FIX] connector_salesforce_server_environment: RST syntax

--- connector_salesforce_server_environment/__openerp__.py
+++ connector_salesforce_server_environment/__openerp__.py
@@ -10,16 +10,16 @@
  'description': """Implements server environment behavior
 for connector Salesforce authentication.
 
- To use it you have to add a section named as:
+To use it you have to add a section named as:
 
-    salesforce_auth_ + Name of the backend
+    salesforce_auth + Name of the backend
 
- Default available section options are:
+Default available section options are:
 
- - authentication_method
- - callback_url
- - sandbox
- - url
+- authentication_method
+- callback_url
+- sandbox
+- url
 
 Module can easily be extended to add any other field.
 By default they are not provided in order not have security issues.
